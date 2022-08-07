40b5ef5194ad5db742d8ab9722789c1679989711
Bhupinder Singh <bhupi98.bs@gmail.com>
2022-08-07 13:58:44 -0700

    Created ship class and gameboard placement function. Created tests for class and gameboard class placement function.

---
+++ battleShipTest.test.js
@@ -0,0 +1,61 @@
+import { Ship, Gameboard } from './src/index';
+
+test('testing constructor function of class ship', () => {
+  const realShip = new Ship(5);
+  expect(realShip.length).toBe(5);
+});
+
+test('testing isSunk function. Expecting a false (not sunk)', () => {
+  const realShip = new Ship(5);
+  expect(realShip.isSunk()).toBe(false);// ship is not sunk
+});
+
+test('testing hit function. Expecting the ship to be sunk after 5 hits', () => {
+  const realShip = new Ship(5);
+  realShip.hit = 0;
+  realShip.hit = 1;
+  realShip.hit = 2;
+  realShip.hit = 3;
+  realShip.hit = 4;
+
+  expect(realShip.isSunk()).toBe(true);
+});
+
+test('testing hit function. Expecting the ship to not be sunk after 5 hits', () => {
+  const realShip = new Ship(5);
+  realShip.hit = 0;
+  realShip.hit = 1;
+  realShip.hit = 2;
+  realShip.hit = 3;
+  realShip.hit = 3;
+
+  expect(realShip.isSunk()).toBe(false);
+});
+
+test('testing Gameboard Class Ship Placement with 0 degree orientation', () => {
+  const gameboard1 = new Gameboard();
+  const realShip = new Ship(8);
+  gameboard1.placeShips(8, 8, 0, realShip);
+  expect(gameboard1.hitPlaces).toStrictEqual([[realShip, '8,8'], [realShip, '9,8'], [realShip, '10,8'], [realShip, '11,8'], [realShip, '12,8'], [realShip, '13,8'], [realShip, '14,8'], [realShip, '15,8']]);
+});
+
+test('testing Gameboard Class Ship Placement with 90 degree orientation', () => {
+  const gameboard1 = new Gameboard();
+  const realShip = new Ship(5);
+  gameboard1.placeShips(1, 1, 90, realShip);
+  expect(gameboard1.hitPlaces).toStrictEqual([[realShip, '1,1'], [realShip, '1,2'], [realShip, '1,3'], [realShip, '1,4'], [realShip, '1,5']]);
+});
+
+test('testing Gameboard Class Ship Placement with 180 degree orientation', () => {
+  const gameboard1 = new Gameboard();
+  const realShip = new Ship(8);
+  gameboard1.placeShips(8, 8, 180, realShip);
+  expect(gameboard1.hitPlaces).toStrictEqual([[realShip, '8,8'], [realShip, '7,8'], [realShip, '6,8'], [realShip, '5,8'], [realShip, '4,8'], [realShip, '3,8'], [realShip, '2,8'], [realShip, '1,8']]);
+});
+
+test('testing Gameboard Class Ship Placement with 270 degree orientation', () => {
+  const gameboard1 = new Gameboard();
+  const realShip = new Ship(5);
+  gameboard1.placeShips(8, 8, 270, realShip);
+  expect(gameboard1.hitPlaces).toStrictEqual([[realShip, '8,8'], [realShip, '8,7'], [realShip, '8,6'], [realShip, '8,5'], [realShip, '8,4']]);
+});
--- src/index.js
+++ src/index.js
@@ -1,22 +1,54 @@
-class ship {
+const DEG_TO_RAD_CONST = Math.PI / 180;
+
+export class Ship {
   constructor(length) {
     this.length = length;
-    const alive = true;
-    let hitLocation;
-    for (let i = 0; i < length; i += 1) {
-      hitLocation[i] = 1;
-      i -= 1;
+    this.alive = true;
+    this.hitLocation = [];
+    for (let i = 0; i < length;) {
+      this.hitLocation[i] = 1;
+      i += 1;
     }
   }
 
-  hit(location) {
+  set hit(location) {
     this.hitLocation[location] = 0;
   }
 
+  get getHitLocationArray() {
+    return this.hitLocation;
+  }
+
   isSunk() {
+    if (!this.alive) { return true; } // is it's sunk no need to go through loops
     const shipStatus = this.hitLocation.every((element) => {
       if (element === 0) { return true; }
+      return false;
     });
-    if (shipStatus) { return true; } return false; //if true the ship is sunk
+    if (shipStatus) {
+      this.alive = false;
+      return true;
+    } return false; // if true the ship is sunk
+  }
+}
+
+export class Gameboard {
+  constructor() {
+    this.hitPlaces = []; // this array contains all the spots where ships are located
+  }
+
+  // places ship on board by filling up hitPlaces array
+  placeShips(xInitial, yInitial, orientation, shipObject) {
+    // using cos and sin to determine how each of the x and y should be incremented
+    const xOrientationConst = Math.round(Math.cos(orientation * DEG_TO_RAD_CONST));
+    const yOrientationConst = Math.round(Math.sin(orientation * DEG_TO_RAD_CONST));
+    // Building hit place array.
+    const tempArray = [];
+    for (let i = shipObject.length; i > 0; i -= 1) {
+      const xNext = xInitial + (shipObject.length - i) * xOrientationConst;
+      const yNext = yInitial + (shipObject.length - i) * yOrientationConst;
+      tempArray.push([shipObject, `${xNext},${yNext}`]);
+    }
+    this.hitPlaces.push(...tempArray);
   }
 }
